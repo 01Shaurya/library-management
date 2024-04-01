d83495d1c2ab3023994671ae5a2d8e28ebe00229
01Shaurya <125733191+01Shaurya@users.noreply.github.com>
2024-04-01 13:00:41 +0530

    Add files via upload

---
+++ circle.py
@@ -0,0 +1,13 @@
+def area_circle():
+    π=3.14159265358979323846
+    radius=int(input("Enter Radius Of Circle: \n"))
+    area=π*(radius**2)
+    print(area)
+    
+
+def circumfrence_cirlce():
+    π=3.14159265358979323846
+    radius=int(input("Enter Radius Of Circle: \n"))
+    circumfrence=π*(radius*2)
+    print(circumfrence)
+
